b551452c2d7062b2c907c9022ba8d66a9e52a95d
achalpatel <achalpatel008@gmail.com>
2020-07-13 17:30:50 -0700

    Comments added

--- patel_achal_assignment1.py
+++ patel_achal_assignment1.py
@@ -1,8 +1,6 @@
 # Assignment 1
 # Name - Achal Patel
 # Student Id - 026598245
-
-
 import numpy as np
 import sys
 np.set_printoptions(threshold=sys.maxsize)
@@ -57,17 +55,19 @@ class Board:
         self.number_matrix=np.array(t)
         
     
-    # This method takes index(i, j) and returns the element at that position from the matrix
+    # This method takes index(i, j) and returns the character at that position from the matrix
     def getElementFromMatrix(self, m, n):
         try:
             return self.matrix[m][n]    
         except IndexError:
             return None
         
-    
+    # This method initializes the Final matrix with zeros
     def initFinalMat(self):
         self.final_matrix=np.zeros((self.col_size*self.row_size, self.col_size*self.row_size))
 
+
+    # This method fills the Final Matrix based on the adjacent moves
     def addValues(self):
         for i in range(0, self.row_size):
             for j in range(0, self.col_size):
@@ -83,79 +83,10 @@ class Board:
                     if(j+1<=self.col_size-1 and self.matrix[i][j+1]!=self.char_obstacle):                       
                        self.final_matrix[num][self.number_matrix[i][j+1]] = 1
 
+    # Prints the final output matrix
     def printFinalMatrix(self):
         for i in range(0, self.final_matrix.shape[0]):
             print("index = ",i, self.final_matrix[i])
-    
-
-    def getStartPos(self):
-        for i in range(0,self.row_size):
-            for j in range(0, self.col_size):
-                if(self.matrix[i][j]==self.char_start):
-                    return self.number_matrix[i][j]
-
-    def getEndPos(self):
-        for i in range(0,self.row_size):
-            for j in range(0, self.col_size):
-                if(self.matrix[i][j]==self.char_end):
-                    return self.number_matrix[i][j]
-
-    def searchDFS(self, startPos, endPos):
-        visited=[]
-        path=[]        
-        visited.append(startPos)
-        for i in range(0, self.final_matrix[startPos].shape[0]):
-            if(self.final_matrix[startPos][i] == 1 and i not in visited):
-                visited.append(i)
-                # print(visited)
-                if(self.search_rec(i, visited, path, endPos)):
-                    path.append(i)
-                    path.append(startPos)
-                    return path
-        
-
-    def search_rec(self, number, visited, path, endPos):
-        if(number==endPos):
-            return True
-        print(number, visited)
-        for i in range(0, self.final_matrix[number].shape[0]):
-            if(self.final_matrix[number][i] == 1 and (i not in visited)):
-                visited.append(i)
-                if(self.search_rec(i, visited, path, endPos)):
-                    path.append(i) 
-                    return True
-        
-        return False
-
-        
-    # Testing purpose
-    def getOnesPos(self):
-        l=[]
-        for i in range(0, self.final_matrix.shape[0]):
-            for j in range(0, self.final_matrix.shape[1]):
-                if(self.final_matrix[i][j]==1):
-                    l.append((i,j))
-        return np.array(l)
-    
-    # Testing purpose
-    def readAnswerMatrix(self, file):
-        f = open(file, 'r')
-        arr=[]
-        for line in f:
-            temp=[]
-            for i in line:
-                if(i!="\n" and i!="\t"):
-                    temp.append(i)
-                td=list(temp)
-            arr.append(td)
-        ans_arr=np.array(arr)
-        ones_list=[]
-        for i in range(0,ans_arr.shape[0]):
-            for j in range(0, ans_arr.shape[1]):
-                if(ans_arr[i][j]=='1'):
-                    ones_list.append((i,j))
-        
-        return np.array(ones_list)
 
 
 b = Board("verySmallMaze.lay","%"," ", "P", ".")
